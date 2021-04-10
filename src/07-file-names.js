306c3ead80baaa01d14834c16909a73f2bb24fa8
antontrmk <antontrmk@gmail.com>
2021-04-10 16:17:14 +0300

    feat: complete tasks partially

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,27 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+// function renameFiles(/* names */) {
+//   throw new Error('Not implemented');
+// }
+
+function renameFiles(names) {
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    if (result.includes(names[i])) {
+      let stopper = true;
+      for (let a = 1; stopper; a++) {
+        if (!result.includes(`${names[i]}(${a})`)) {
+          stopper = false;
+          result.push(`${names[i]}(${a})`);
+        }
+      }
+    } else {
+      result.push(names[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = renameFiles;
+// console.log(renameFiles(["file", "file", "image", "file(1)", "file"]));
--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -22,8 +22,28 @@
 //   return [...set].join('');
 // }
 
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+// function encodeLine(/* str */) {
+//   throw new Error('Not implemented');
+// }
+
+function encodeLine(str) {
+  const result = [];
+  let factor = 1;
+  let index = -1;
+  const arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    if (`${factor}${arr[i]}` === result[index]) {
+      factor += 1;
+      result[index] = `${factor}${arr[i]}`;
+    } else {
+      index += 1;
+      factor = 1;
+      result[index] = `${factor}${arr[i]}`;
+    }
+  }
+
+  return result.join('').split('').filter((i) => i !== '1').join('');
 }
 
 module.exports = encodeLine;
