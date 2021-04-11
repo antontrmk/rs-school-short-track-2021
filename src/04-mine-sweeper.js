6ce3973734f3a9df4f7021236eeecdecfbb3168e
antontrmk <antontrmk@gmail.com>
2021-04-11 22:23:16 +0300

    feat: minesweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,37 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  let sum = 0;
+  const resultArr = matrix.map((item, index, arr) => arr.slice());
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      sum = 0;
+
+      const rowStart = i - 1 < 0 ? 0 : i - 1;
+      const colStart = j - 1 < 0 ? 0 : j - 1;
+      const rowEnd = i + 1 > matrix.length - 1 ? matrix.length - 1 : i + 1;
+      const colEnd = j + 1 > matrix[i].length - 1 ? matrix[i].length - 1 : j + 1;
+
+      for (let r = rowStart; r <= rowEnd; r++) {
+        for (let c = colStart; c <= colEnd; c++) {
+          if (r !== i || c !== j) {
+            if (matrix[r][c] === true) {
+              sum += 1;
+            }
+          }
+        }
+      }
+      resultArr[i][j] = sum;
+    }
+  }
+  return resultArr;
 }
 
 module.exports = minesweeper;
+
+// console.log(minesweeper([
+//   [true, false, false],
+//   [false, true, false],
+//   [false, false, false],
+// ]));
